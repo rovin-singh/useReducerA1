b4c98de8f5761902472128bc75b24a7d61c97f9b
rovin-singh <91409265+rovin-singh@users.noreply.github.com>
2022-06-01 21:01:37 +0530

    "commit"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import { useReducer, useState } from "react";
 import "./styles.css";
-
-export default function App() {
+function reducer(state, action) {
+  switch (action.type) {
+    case "addTodo":
+      return {
+        todos: [
+          ...state.todos,
+          { text: action.text, completed: false, id: state.todos.length }
+        ]
+      };
+    default:
+      return state;
+  }
+}
+function App() {
+  const [{ todos }, dispatch] = useReducer(reducer, { todos: [] });
+  const [text, setText] = useState("");
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch({ type: "addTodo", text });
+          setText("");
+        }}
+      >
+        <input
+          type="text"
+          value={text}
+          placeholder="Enter Something"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <input type="submit" value="add" />
+      </form>
+
+      {todos.map((data) => (
+        <div key={data.id}>
+          <p>
+            {data.text}: {data.id}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
+export default App;
